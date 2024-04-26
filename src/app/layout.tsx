58e9abc3bcd875b4265fb392c0bc326bb731dab2
MAMCB <96302464+MAMCB@users.noreply.github.com>
2024-04-26 17:57:05 +0200

    added navbar with links for each page, removed default global css styles,added basic styling to navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +17,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav className="p-8 mb-8 bg-gray-300">
+          <ul className="flex justify-evenly">
+            <li className="bg-transparent p-5 rounded-xl shadow-xl">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="bg-transparent p-5 rounded-xl shadow-xl">
+              <Link href="/performance">Performance</Link>
+            </li>
+            <li className="bg-transparent p-5 rounded-xl shadow-xl">
+              <Link href="/reliability">Reliability</Link>
+            </li>
+            <li className="bg-transparent p-5 rounded-xl shadow-xl">
+              <Link href="/scale">Scale</Link>
+            </li>
+          </ul>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
